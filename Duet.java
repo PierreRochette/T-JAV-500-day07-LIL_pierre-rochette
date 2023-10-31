6c86d49d9392d17ffaa698f2e0096b583dff672d
Pierre Rochette <pierre.rochette@epitech.eu>
2023-10-31 11:24:21 +0100

    task3

---
+++ Duet.java
@@ -0,0 +1,11 @@
+public class Duet {
+
+    public static <T extends Comparable<T>> T min(T a, T b) {
+        return (a.compareTo(b) < 0) ? a : b ;
+    }
+
+    public static <T extends Comparable<T>> T max (T a, T b) {
+        return (a.compareTo(b) > 0) ? a : b ;
+    }
+
+}
